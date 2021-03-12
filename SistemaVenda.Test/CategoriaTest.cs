f99d21bfea6206ab89c57237616b70371f216098
Anderson Junior Rodrigues <andersonkz@ymail.com>
2021-03-12 09:00:31 -0300

    Implementação de teste unitários

---
+++ SistemaVenda.Test/CategoriaTest.cs
@@ -0,0 +1,84 @@
+﻿using AutoFixture;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SistemaVenda.Dominio.Entidades;
+using SistemaVenda.Dominio.Interface;
+using SistemaVenda.Dominio.Repositorio;
+using SistemaVenda.Dominio.Servicos;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SistemaVenda.Test
+{
+    [Collection("Categoria")]
+    public class CategoriaTest
+    {
+        private Mock<ICategoriaRepositorio> _categoriaRepositorio;
+        private ICategoriaServico _categoriaServico;
+        private ILogger<CategoriaServico> _loggerCategoria;
+        private IList<Categoria> _categorias;
+        private Categoria _categoria;
+        private Fixture _fixture;
+
+        public CategoriaTest()
+        {
+            _loggerCategoria = new LoggerFactory().CreateLogger<CategoriaServico>();
+            _categoriaRepositorio = new Mock<ICategoriaRepositorio>();
+            _categoriaServico = new CategoriaServico(_categoriaRepositorio.Object, _loggerCategoria);
+            _fixture = new Fixture();
+        }
+
+        [Fact]
+        public async Task GetCategoriasTest()
+        {
+            _categorias = new List<Categoria>()
+            {
+                { new Fixture().Create<Categoria>() },
+                { new Fixture().Create<Categoria>() },
+                { new Fixture().Create<Categoria>() },
+                { new Fixture().Create<Categoria>() }
+            };
+
+            var categoriaRepositorioMock = _categoriaRepositorio.Setup(c => c.GetAll()).ReturnsAsync(() => _categorias);
+            var categoriaResult = await _categoriaServico.GetCategorias();
+
+            Assert.True(categoriaResult.Any());
+        }
+
+        [Fact]
+        public async Task GetCategoriaTest()
+        {
+            _categoria = _fixture.Create<Categoria>();
+
+            var categoriaRepositorioMock = _categoriaRepositorio.Setup(c => c.Get(It.IsAny<int>())).ReturnsAsync((int codigo) => _categoria);
+            var categoriaResult = await _categoriaServico.GetCategoria(_categoria.Codigo);
+
+            Assert.Equal(categoriaResult.Codigo, _categoria.Codigo);
+            Assert.Equal(categoriaResult.Descricao, _categoria.Descricao);
+        }
+
+        [Fact]
+        public async Task SalvarCategoriaTest()
+        {
+            _categoria = _fixture.Create<Categoria>();
+
+            var categoriaRepositorioMock = _categoriaRepositorio.Setup(c => c.Add(It.IsAny<Categoria>())).ReturnsAsync(() => _categoria);
+            await _categoriaServico.SaveCategoria(_categoria);
+
+            Assert.True(true);
+        }
+
+        [Fact]
+        public async Task EditarCategoriaTest()
+        {
+            _categoria = _fixture.Create<Categoria>();
+
+            var categoriaRepositorioMock2 = _categoriaRepositorio.Setup(c => c.Update(It.IsAny<Categoria>())).ReturnsAsync(() => _categoria);
+            await _categoriaServico.EditarCategoria(_categoria);
+
+            Assert.True(true);
+        }
+    }
+}
---
+++ SistemaVenda.Test/UsuarioTest.cs
@@ -0,0 +1,45 @@
+using Moq;
+using SistemaVenda.Dominio.Entidades;
+using SistemaVenda.Dominio.Repositorio;
+using Xunit;
+using AutoFixture;
+using System.Threading.Tasks;
+using SistemaVenda.Dominio.Servicos;
+using SistemaVenda.Dominio.Interface;
+using Microsoft.Extensions.Logging;
+
+namespace SistemaVenda.Test
+{
+    [Collection("Usuario")]
+    public class UsuarioTest
+    {
+        private Mock<IUsuarioRepositorio> _usuarioRepositorio;
+        private IUsuarioServico _usuarioServico;
+        private ILogger<UsuarioServico> _logger;
+        private Usuario _usuario;
+        private Fixture _fixture;
+
+        public UsuarioTest()
+        {
+            _logger = new LoggerFactory().CreateLogger<UsuarioServico>();
+            _usuarioRepositorio = new Mock<IUsuarioRepositorio>();
+            _usuarioServico = new UsuarioServico(_usuarioRepositorio.Object, _logger);
+            _fixture = new Fixture();
+        }
+
+        [Fact]
+        public async Task AutenticaUsuarioTest()
+        {
+            _usuario = _fixture.Create<Usuario>();
+
+            var usuarioRepositorioMock = _usuarioRepositorio.Setup(u => u.GetUsuarioAutentication(It.IsAny<string>(), It.IsAny<string>()))
+                                                            .ReturnsAsync((string email, string senha) => _usuario);
+
+            var usuarioResult = await _usuarioServico.GetUsuarioAutenticacao(_usuario.Email, _usuario.Senha);
+
+            Assert.Equal(usuarioResult.Email, _usuario.Email);
+            Assert.Equal(usuarioResult.Senha, _usuario.Senha);
+            Assert.Equal(usuarioResult.Nome, _usuario.Nome);
+        }
+    }
+}
